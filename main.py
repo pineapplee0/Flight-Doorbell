235800b6e1a028101737c12d3fbd5ba253ac35c3
Katelyn Brown <katelynelisebrown0212@gmail.com>
2022-08-16 20:17:59 -0500

    add distFrom func, refactor imports

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
+#library imports
+import sys
+import json
+import datetime
+import math
+
+#store credentials as var
 import config
 
-import pydub
-from pydub import alert
-from pydub.playback import play
-alert = chime.from_mp3('chime.mp3')
-play(alert);
-
-import json
-
-import datetime
-time = datetime.time();
-
-import sys
 for line in sys.stdin
 
 def checkAircraft(aircraft):
     distance = distFrom(aircraft);
-
     matches = favorites.filter #finish off the rest of this line
     return airframes in aircraft.aircraftId
-
-isSpecial = 
-    matches.len() True = and
-    aircraft.altitude #finish off this line and next line
-    #finish
+    isSpecial = 
+        len(matches) != 0 and
+        aircraft.altitude <= thresholds.altitude and
+        distance <= thresholds.distance
 
 print(f'Testing aircraft: {json.dumps(aircraft)}');
 print(recentAircraft); 
 
-isSpecial {
-        print(f'Important plane detected {distance} km away! Aircraft ID: {aircraft.aircraftId}. Group colors: {matches[0].colors}');
-        play(alert)
+if (isSpecial):
+    print(f'Important plane detected {distance} km away! Aircraft ID: {aircraft.aircraftId}. Group colors: {matches[0].colors}');
+
+def distFrom(aircraft):
+    R = 6371 # earth radius in km
+    homeLat = config.homeCoords[0]
+    homeLon = config.homeCoords[1]
+    lat = aircraft.coordinatePair[0]
+    lon = aircraft.coordinatePair[1]
+    dlat = toRad(lat-homeLat)
+    dLon = toRad(lon-homeLon)
+    lat1 = toRad(homeLat)
+    lat2 = toRad()
+
+    a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.sin(dLon / 2) * math.sin(dLon / 2) * math.cos(lat1) * math.cos(lat2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    d = R * c
+    return round(d, 1)
+
+def toRad(value):
+    return value * math.pi / 180 
