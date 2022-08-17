dfd16081578bb40eb188011a955dc60abf88eeca
Katelyn Brown <katelynelisebrown0212@gmail.com>
2022-08-16 19:23:14 -0500

    initial test commit of main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import config
+
+import pydub
+from pydub import alert
+from pydub.playback import play
+alert = chime.from_mp3('chime.mp3')
+play(alert);
+
+import json
+
+import datetime
+time = datetime.time();
+
+import sys
+for line in sys.stdin
+
+def checkAircraft(aircraft):
+    distance = distFrom(aircraft);
+
+    matches = favorites.filter #finish off the rest of this line
+    return airframes in aircraft.aircraftId
+
+isSpecial = 
+    matches.len() True = and
+    aircraft.altitude #finish off this line and next line
+    #finish
+
+print(f'Testing aircraft: {json.dumps(aircraft)}');
+print(recentAircraft); 
+
+isSpecial {
+        print(f'Important plane detected {distance} km away! Aircraft ID: {aircraft.aircraftId}. Group colors: {matches[0].colors}');
+        play(alert)
