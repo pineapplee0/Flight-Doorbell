8555dfb36c97fa379e8ddcdd11193d76a38dd441
Katelyn Brown <katelynelisebrown0212@gmail.com>
2022-08-20 01:44:16 -0500

    implemented timeDiff, made minor edits to js syntax

--- main.py
+++ main.py
@@ -12,17 +12,19 @@ recentAircraft = {}
 
 for line in sys.stdin:
     msg = line.split(",")
-    if (msg[4] && msg[11] && msg[14] && msg[15]):
+    if (msg[4] and msg[11] and msg[14] and msg[15]):
         recentAircraft[msg[4]] = datetime.datetime.now()
+        timeDiff = recentAircraft[msg[4]] - datetime.datetime.now()
+
+        if timeDiff.total_seconds() >= 3:
+            print("deleting... {msg[4]}")
+            del recentAircraft[4] 
 
 def checkAircraft(aircraft):
     distance = distFrom(aircraft);
-    matches = favorites.filter #finish off the rest of this line
+    matches = favorites.filter
     return airframes in aircraft.aircraftId
-    isSpecial = 
-        len(matches) != 0 and
-        aircraft.altitude <= thresholds.altitude and
-        distance <= thresholds.distance
+    isSpecial = len(matches) != 0 and aircraft.altitude <= thresholds.altitude and distance <= thresholds.distance
 
 print(f'Testing aircraft: {json.dumps(aircraft)}');
 print(recentAircraft); 
@@ -30,6 +32,7 @@ print(recentAircraft);
 if (isSpecial):
     print(f'Important plane detected {distance} km away! Aircraft ID: {aircraft.aircraftId}. Group colors: {matches[0].colors}');
 
+#calculating distance of aircraft from homeCoords in km
 def distFrom(aircraft):
     R = 6371 # earth radius in km
     homeLat = config.homeCoords[0]
